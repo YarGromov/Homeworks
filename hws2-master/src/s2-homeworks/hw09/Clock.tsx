8a4a8d583545a3ee2ff8cad75d5b68db8e0d1894
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-12-19 17:49:09 +0100

    hw9

--- hws2-master/src/s2-homeworks/hw09/Clock.tsx
+++ hws2-master/src/s2-homeworks/hw09/Clock.tsx
@@ -42,13 +42,14 @@ function Clock() {
 
 
 
-    const stringDate = date?.toLocaleDateString('en-UK', {
+    const stringDate = date?.toLocaleDateString('ru', {
         day: "numeric",
         month: "numeric",
         year: "numeric"
     }) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
+
 
 
 
